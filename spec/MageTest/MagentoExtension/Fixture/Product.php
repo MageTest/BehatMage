5546eb7b32d81ff30939550efb132c9007478065
Sarunas Valaskevicius <svalaskevicius@inviqa.com>
2012-09-22 15:14:16 +0100

    create spec for create product

--- spec/MageTest/MagentoExtension/Fixture/Product.php
+++ spec/MageTest/MagentoExtension/Fixture/Product.php
@@ -6,8 +6,25 @@ use PHPSpec2\Specification;
 
 class Product implements Specification
 {
-    function it_should_exist()
+    function it_should_create_a_product_given_all_required_attributes()
     {
-        $this->object->shouldNotBe(null);
+        $this->product->create(
+            array(
+                'sku' => 'sku1'
+            )
+        );
+        $productData = $this->_retrieveProductData('sku1');
+        if ('sku1' !== $productData['sku']) {
+            throw new \Exception("sku retrieved is not correct <sku1> !== <{$productData['sku']}>");
+        }
+    }
+
+    protected function _retrieveProductData($sku)
+    {
+        $product = \Mage::getModel('catalog/product')->loadByAttribute('sku', $sku);
+        if (!$product || !$product->getId()) {
+            throw new \Exception("cannot find product by given sku <$sku>");
+        }
+        return $product->getData();
     }
 }
