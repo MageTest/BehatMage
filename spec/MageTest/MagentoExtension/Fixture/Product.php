7a63a08bc95c7525bfc58b3ea0b81a6039b10097
Sarunas Valaskevicius <svalaskevicius@inviqa.com>
2012-09-22 19:32:04 +0100

    add default attribute values

--- spec/MageTest/MagentoExtension/Fixture/Product.php
+++ spec/MageTest/MagentoExtension/Fixture/Product.php
@@ -20,8 +20,20 @@ class Product implements Specification
     function it_should_create_a_product_given_all_required_attributes()
     {
         $data = array(
-            'sku' => 'sku1'
+            'attribute_set_id' => 7,
+            'name' => 'product name',
+            'weight' => 2,
+            'visibility'=> \Mage_Catalog_Model_Product_Visibility::VISIBILITY_BOTH,
+            'status' => \Mage_Catalog_Model_Product_Status::STATUS_ENABLED,
+            'price' => 100,
+            'description' => 'Product description',
+            'short_description' => 'Product short description',
+            'tax_class_id' => 1,
+            'type_id' => \Mage_Catalog_Model_Product_Type::TYPE_SIMPLE,
+            'stock_data' => array( 'is_in_stock' => 1, 'qty' => 99999 ),
+            'sku' => 'sdf'
         );
+
         $this->productModel->shouldReceive('setData')
             ->with($data)->once()->andReturn($this->productModel)->ordered();
         $this->productModel->shouldReceive('save')->once()->andReturn(true)->ordered();
@@ -32,21 +44,28 @@ class Product implements Specification
     function it_should_populate_missing_attributes_when_creating_product()
     {
         $data = array(
-            'attribute_set_id' => 9,
-            'sku' => 'sadsd',
-            'name' => 'ad',  
-            'weight' => 2,
-            'visibility'=>4,
-            'status'=>1,
-            'price'=>1,
-            'description'=>'asdsad',
-            'short_description'=>'sdf',
-            'qty'=>3, 
-            'tax_class_id'=>1,
-            'type_id'=>\Mage_Catalog_Model_Product_Type::TYPE_SIMPLE,
-            'stock_data' =>array( 'is_in_stock' => 1, 'qty' => 99999 )
+            'sku' => 'sku1'
         );
+
+        $expected = array(
+            'attribute_set_id' => 9,
+            'name' => 'product name',
+            'weight' => 2,
+            'visibility'=> \Mage_Catalog_Model_Product_Visibility::VISIBILITY_BOTH,
+            'status' => \Mage_Catalog_Model_Product_Status::STATUS_ENABLED,
+            'price' => 100,
+            'description' => 'Product description',
+            'short_description' => 'Product short description',
+            'tax_class_id' => 1,
+            'type_id' => \Mage_Catalog_Model_Product_Type::TYPE_SIMPLE,
+            'stock_data' => array( 'is_in_stock' => 1, 'qty' => 99999 ),
+            'sku' => 'sku1'
+        );
+
+        $this->productModel->shouldReceive('setData')
+            ->with($expected)->once()->andReturn($this->productModel)->ordered();
+        $this->productModel->shouldReceive('save')->once()->andReturn(true)->ordered();
 
         $this->product->create($data);
     }
-}    
+}
--- src/MageTest/MagentoExtension/Fixture/Product.php
+++ src/MageTest/MagentoExtension/Fixture/Product.php
@@ -31,6 +31,8 @@ class Product
         $modelFactory = $this->_modelFactory;
         $model = $modelFactory();
 
+        $attributes = array_merge($this->_getDefaultAttributes($model), $attributes);
+
         \Mage::app()->setCurrentStore(\Mage_Core_Model_App::ADMIN_STORE_ID);
         $model->setData($attributes)->save();
         \Mage::app()->setCurrentStore(\Mage_Core_Model_App::DISTRO_STORE_ID);
@@ -46,5 +48,22 @@ class Product
         return function () {
             return \Mage::getModel('catalog/product');
         };
+    }
+
+    protected function _getDefaultAttributes($model)
+    {
+        return array(
+            'attribute_set_id' => $model->getResource()->getEntityType()->getDefaultAttributeSetId(),
+            'name' => 'product name',
+            'weight' => 2,
+            'visibility'=> \Mage_Catalog_Model_Product_Visibility::VISIBILITY_BOTH,
+            'status' => \Mage_Catalog_Model_Product_Status::STATUS_ENABLED,
+            'price' => 100,
+            'description' => 'Product description',
+            'short_description' => 'Product short description',
+            'tax_class_id' => 1,
+            'type_id' => \Mage_Catalog_Model_Product_Type::TYPE_SIMPLE,
+            'stock_data' => array( 'is_in_stock' => 1, 'qty' => 99999 )
+        );
     }
 }
