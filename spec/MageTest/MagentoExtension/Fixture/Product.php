06e759b572222b4b076635e8078a4eb80a4672c5
Sarunas Valaskevicius <svalaskevicius@inviqa.com>
2012-09-22 16:17:06 +0100

    add product fixture creation

--- spec/MageTest/MagentoExtension/Fixture/Product.php
+++ spec/MageTest/MagentoExtension/Fixture/Product.php
@@ -6,25 +6,26 @@ use PHPSpec2\Specification;
 
 class Product implements Specification
 {
+    function described_with()
+    {
+        \Mage::app();
+        $this->productModel = \Mockery::mock(new \Mage_Catalog_Model_Product());
+
+        $productModel = $this->productModel;
+
+        $factory = function () use ($productModel) { return $productModel; };
+        $this->product->isAnInstanceOf('MageTest\MagentoExtension\Fixture\Product', array($factory));
+    }
+
     function it_should_create_a_product_given_all_required_attributes()
     {
+        $this->productModel->shouldReceive('setData')->once()->andReturn($this->productModel)->ordered();
+        $this->productModel->shouldReceive('save')->once()->andReturn(true)->ordered();
+
         $this->product->create(
             array(
                 'sku' => 'sku1'
             )
         );
-        $productData = $this->_retrieveProductData('sku1');
-        if ('sku1' !== $productData['sku']) {
-            throw new \Exception("sku retrieved is not correct <sku1> !== <{$productData['sku']}>");
-        }
-    }
-
-    protected function _retrieveProductData($sku)
-    {
-        $product = \Mage::getModel('catalog/product')->loadByAttribute('sku', $sku);
-        if (!$product || !$product->getId()) {
-            throw new \Exception("cannot find product by given sku <$sku>");
-        }
-        return $product->getData();
     }
 }
--- src/MageTest/MagentoExtension/Fixture/Product.php
+++ src/MageTest/MagentoExtension/Fixture/Product.php
@@ -9,15 +9,40 @@ namespace MageTest\MagentoExtension\Fixture;
  */
 class Product
 {
+    private $_modelFactory = null;
+
+    /**
+     * @param $productModelFactory \Closure optional
+     */
+    public function __construct($productModelFactory = null)
+    {
+        $this->_modelFactory = $productModelFactory ?: $this->defaultModelFactory();
+    }
+
     /**
      * Create a product fixture using the given attributes map
      *
-     * @param array $attributes product attributes map using 'label' => 'value' format
+     * @param $attributes array product attributes map using 'label' => 'value' format
      *
      * @return null
      */
     public function create($attributes)
     {
+        $modelFactory = $this->_modelFactory;
 
+        $model = $modelFactory();
+        $model->setData($attributes)->save();
+    }
+
+    /**
+     * retrieve default product model factory
+     *
+     * @return \Closure
+     */
+    public function defaultModelFactory()
+    {
+        return function () {
+            return \Mage::getModel('catalog/product');
+        };
     }
 }
