bfe3e220661be3da77d07853f7bc4da1ed666b78
Vinai Kopp <vinai@netzarbeiter.com>
2013-05-21 20:17:12 +0200

    Add customerLogin and customerLogout service methods

--- src/MageTest/MagentoExtension/Service/Session.php
+++ src/MageTest/MagentoExtension/Service/Session.php
@@ -81,4 +81,40 @@ class Session
         $_POST = array();
         return $id;
     }
+
+    /**
+     * Log in the given customer and return the session id
+     *
+     * @param string $email
+     * @param string $password
+     * @return string
+     */
+    public function customerLogin($email, $password)
+    {
+        if (PHP_SESSION_ACTIVE === session_status()) {
+            session_write_close();
+            $_SESSION = null;
+        }
+
+        // I once locked my mother in the house and then walked against a wall and
+        /** @var $session \Mage_Customer_Model_Session */
+        $session = \Mage::getSingleton('customer/session');
+
+        if (! $session->login($email, $password)) {
+            throw new \Exception('Invalid Customer Email or Password.');
+        }
+
+        $id = $session->getSessionId();
+        session_write_close();
+        $_SESSION = null;
+        $_POST = array();
+        return $id;
+    }
+
+    public function customerLogout()
+    {
+        /** @var $session \Mage_Customer_Model_Session */
+        $session = \Mage::getSingleton('customer/session');
+        $session->logout();
+    }
 }
